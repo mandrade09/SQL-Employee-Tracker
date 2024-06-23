35350a6898de3f139a6973d291ed4be5b6d666c0
Matthew  Andrade <mattandrade09@gmail.com>
2024-06-23 18:57:22 -0500

    Added Video Link

--- employee-management/index.js
+++ employee-management/index.js
@@ -4,7 +4,7 @@ const { getAllRoles, addRole } = require('./queries/roles');
 const { getAllEmployees, addEmployee, updateEmployeeRole } = require('./queries/employees');
 
 async function mainMenu() {
-  console.log('Main menu is being called'); // Debugging statement
+  console.log('Main menu is being called');
   const { action } = await inquirer.prompt([
     {
       type: 'list',
@@ -23,7 +23,7 @@ async function mainMenu() {
     },
   ]);
 
-  console.log(`Action selected: ${action}`); // Debugging statement
+  console.log(`Action selected: ${action}`);
 
   switch (action) {
     case 'View all departments':
@@ -82,58 +82,59 @@ async function mainMenu() {
       const { firstName, lastName, employeeRoleId, employeeManagerId } = await inquirer.prompt([
         {
           type: 'input',
-        name: 'firstName',
-        message: 'Enter the first name of the employee:',
-      },
-      {
-        type: 'input',
-        name: 'lastName',
-        message: 'Enter the last name of the employee:',
-      },
-      {
-        type: 'list',
-        name: 'employeeRoleId',
-        message: 'Select the role of the employee:',
-        choices: roleChoices,
-      },
-      {
-        type: 'list',
-        name: 'employeeManagerId',
-        message: 'Select the manager of the employee:',
-        choices: managerChoices,
-      },
-    ]);
-    await addEmployee(firstName, lastName, employeeRoleId, employeeManagerId);
-    console.log('Employee added successfully.');
-    break;
-  case 'Update an employee role':
-    const employeesForUpdate = await getAllEmployees();
-    const roleChoicesForUpdate = rolesForEmployee.map(role => ({ name: role.title, value: role.id }));
-    const employeeChoicesForUpdate = employeesForUpdate.map(emp => ({ name: `${emp.first_name} ${emp.last_name}`, value: emp.id }));
-    const { employeeId, newRoleId } = await inquirer.prompt([
-      {
-        type: 'list',
-        name: 'employeeId',
-        message: 'Select the employee to update:',
-        choices: employeeChoicesForUpdate,
-      },
-      {
-        type: 'list',
-        name: 'newRoleId',
-        message: 'Select the new role for the employee:',
-        choices: roleChoicesForUpdate,
-      },
-    ]);
-    await updateEmployeeRole(employeeId, newRoleId);
-    console.log('Employee role updated successfully.');
-    break;
-  case 'Exit':
-    console.log('Exiting application'); // Debugging statement
-    process.exit();
+          name: 'firstName',
+          message: 'Enter the first name of the employee:',
+        },
+        {
+          type: 'input',
+          name: 'lastName',
+          message: 'Enter the last name of the employee:',
+        },
+        {
+          type: 'list',
+          name: 'employeeRoleId',
+          message: 'Select the role of the employee:',
+          choices: roleChoices,
+        },
+        {
+          type: 'list',
+          name: 'employeeManagerId',
+          message: 'Select the manager of the employee:',
+          choices: managerChoices,
+        },
+      ]);
+      await addEmployee(firstName, lastName, employeeRoleId, employeeManagerId);
+      console.log('Employee added successfully.');
+      break;
+    case 'Update an employee role':
+      const rolesForUpdate = await getAllRoles(); // Initialize rolesForUpdate here
+      const employeesForUpdate = await getAllEmployees();
+      const roleChoicesForUpdate = rolesForUpdate.map(role => ({ name: role.title, value: role.id })); // Now reference rolesForUpdate
+      const employeeChoicesForUpdate = employeesForUpdate.map(emp => ({ name: `${emp.first_name} ${emp.last_name}`, value: emp.id }));
+      const { employeeId, newRoleId } = await inquirer.prompt([
+        {
+          type: 'list',
+          name: 'employeeId',
+          message: 'Select the employee to update:',
+          choices: employeeChoicesForUpdate,
+        },
+        {
+          type: 'list',
+          name: 'newRoleId',
+          message: 'Select the new role for the employee:',
+          choices: roleChoicesForUpdate,
+        },
+      ]);
+      await updateEmployeeRole(employeeId, newRoleId);
+      console.log('Employee role updated successfully.');
+      break;
+    case 'Exit':
+      console.log('Exiting application');
+      process.exit();
   }
 
   mainMenu();
 }
 
-console.log('Starting application'); // Debugging statement
+console.log('Starting application');
 mainMenu();
